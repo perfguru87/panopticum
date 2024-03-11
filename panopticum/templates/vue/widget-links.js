d35d02d32e492eb2fc455fb699c2f061e769772f
Alexander Andreev <aa@acronis.com>
2024-03-11 03:33:59 +0200

    Added automated links replacement in the component summary

---
+++ panopticum/templates/vue/widget-links.js
@@ -0,0 +1,29 @@
+Vue.component('widget-links', {
+  props: {
+    // Define the "text" prop expected from the parent
+    text: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    linkedText() {
+      let ret = this.text
+        .split(/\s+/) // Split by whitespace
+        .map(part => {
+          // Basic URL detection (for demonstration; consider more robust methods for production)
+          if (part.startsWith('http://') || part.startsWith('https://')) {
+            return `<a href="${part}" target="_blank" title='${part}'><i class='fa fa-edit'></i></a>`;
+          }
+          return part;
+        })
+        .join(' '); // Rejoin the parts into a single string
+      return ret;
+    }
+  },
+  template: `
+  {% verbatim %}
+        <span v-html="linkedText"></span>
+  {% endverbatim %}
+  `
+})
