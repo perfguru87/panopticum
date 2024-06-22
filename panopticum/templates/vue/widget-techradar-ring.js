3e12e2646bff109bc616b2ae6685bd53639f6731
Alexander Andreev <aa@acronis.com>
2024-06-22 12:08:54 +0300

    Fix race condition in tech radar loading

--- panopticum/templates/vue/widget-techradar-ring.js
+++ panopticum/templates/vue/widget-techradar-ring.js
@@ -1,6 +1,6 @@
 Vue.component('widget-techradar-ring', {
     props: [
-      'attrib',
+        'attrib',
     ],
     data: function() {
         return {
@@ -9,56 +9,57 @@ Vue.component('widget-techradar-ring', {
         }
     },
     created: async function() {
-        Promise.all([this.initQuadrants(), this.initRings()]).then(() => {
+        try {
+            await Promise.all([this.initQuadrants(), this.initRings()]);
             this.initEntries();
-        }).catch(err => {
+        } catch (err) {
             console.error('Initialization of quadrants or rings failed', err);
-        });
+        }
     },
     computed: {
     },
     methods: {
         initQuadrants: async function() {
-           fetch('/api/techradar_quadrants/').then(function(response) {
-             return response.json();
-           }).then(function(data) {;
-             this.quadrants = data['results'];
-           }).catch(function(err) {
-             console.log('Error loading techradar quadrants', err);
-           });
+            try {
+                const response = await fetch('/api/techradar_quadrants/');
+                const data = await response.json();
+                this.quadrants = data['results'];
+            } catch (err) {
+                console.log('Error loading techradar quadrants', err);
+            }
         },
         initRings: async function() {
-           fetch('/api/techradar_rings/').then(function(response) {
-             return response.json();
-           }).then(function(data) {;
-             this.rings = data['results'];
-           }).catch(function(err) {
-             console.log('Error loading techradar rings', err);
-           });
+            try {
+                const response = await fetch('/api/techradar_rings/');
+                const data = await response.json();
+                this.rings = data['results'];
+            } catch (err) {
+                console.log('Error loading techradar rings', err);
+            }
         },
-        initEntries() {
-           fetch('/api/techradar_entries/').then(function(response) {
-             return response.json();
-           }).then(function(data) {
-             radar_visualization({
-               svg_id: "radar",
-               width: 1250,
-               height: 800,
-               colors: {
-                 background: "#fff",
-                 grid: '#dddde0',
-                 inactive: "#ddd"
-               },
-               quadrants: this.quadrants,
-               rings: this.rings,
-               print_layout: true,
-               links_in_new_tabs: true,
-               // zoomed_quadrant: 0,
-               entries: data.results
-             });
-           }).catch(function(err) {
-             console.log('Error loading techradar entries', err);
-           });
+        initEntries: async function() {
+            try {
+                const response = await fetch('/api/techradar_entries/');
+                const data = await response.json();
+                radar_visualization({
+                    svg_id: "radar",
+                    width: 1250,
+                    height: 800,
+                    colors: {
+                        background: "#fff",
+                        grid: '#dddde0',
+                        inactive: "#ddd"
+                    },
+                    quadrants: this.quadrants,
+                    rings: this.rings,
+                    print_layout: true,
+                    links_in_new_tabs: true,
+                    // zoomed_quadrant: 0,
+                    entries: data.results
+                });
+            } catch (err) {
+                console.log('Error loading techradar entries', err);
+            }
         }
     },
     template: `
@@ -66,4 +67,4 @@ Vue.component('widget-techradar-ring', {
         <svg id="radar" viewBox="0 0 1250 800" preserveAspectRatio="xMidYMid meet" style="width: 100%; height: auto; left: -20px; position: relative;"></svg>
     {% endverbatim %}
     `
-})
+});
